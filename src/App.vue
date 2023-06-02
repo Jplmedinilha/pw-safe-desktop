372b91f8242513666c3ad955c4443ea6bc4294e7
JMEDINILHA <hae.joao.lmedinilha@hyundai-brasil.com>
2023-06-02 20:24:21 -0300

    first commmit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+          @click="teste"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        @click="teste"
+      >
+        oi
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
+// import authHeader from "../axios/auth-header";
+import authHeader from "./axios/auth-header";
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    teste_5: []
+  }),
+  methods:{
+    async teste(){
+
+      await this.axios.get(
+        `http://localhost:3000/api/login?username=JMEDINILHA&pw=jplm12345`, { headers: authHeader(true) }
+      )
+      .then((res) =>{
+        console.log(res)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+
+      console.log('oi');
+    }
+  }
+};
+</script>
---
+++ src/axios/httpsAgent.js
@@ -0,0 +1,8 @@
+// import https from "https";
+
+// export default function httpsCustomAgent() {
+//   //https.globalAgent.options.rejectUnauthorized = false;
+//   return new https.Agent({
+//     rejectUnauthorized: false,
+//   });
+// }
